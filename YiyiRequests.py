f77cc77632d6225e07c6aade290764b51a744141
topliugang <335762225@qq.com>
2019-11-08 16:57:26 +0800

    parse ebook html

--- YiyiRequests.py
+++ YiyiRequests.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import scrapy
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
+import PyV8
+import re
 
 
 #不想要302重定向直接跳转问题：在get方法里面增加allow_redirects=False
@@ -26,9 +28,35 @@ if __name__ == '__main__':
     # span = Selector(response=r).css('span.zli_i_web')[0]
     # print span.css('a::attr(href)').get()
 
-    pdfreader_url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhqwbn0xfqpxuv6wkk.fgzi.wap.gxlib.org/reader/pdf/pdfreader?ssid=96178226&d=791d243c6fd684058ff733a50dd4af24'
-    r = yiyi_get(pdfreader_url)
-    span = Selector(response=r).css('span.zli_i_web')[0]
+    # pdfreader_url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhqwbn0xfqpxuv6wkk.fgzi.wap.gxlib.org/reader/pdf/pdfreader?ssid=96178226&d=791d243c6fd684058ff733a50dd4af24'
+    # r = yiyi_get(pdfreader_url)
+    # span = Selector(response=r).css('span.zli_i_web')[0]
+
+    with open('test.html', 'r') as html_file:
+        html_text = html_file.read()
+        selector = Selector(text=html_text)
+        scripts = selector.css('script')
+        js = scripts[3].extract()[8:-9].strip()
+
+        filemark = re.search('var fileMark = "\d*";', js).group()[16:-2]
+        #print filemark
+        usermark = re.search('var userMark = "\d*";', js).group()[16:-2]
+        #print '_'+usermark+'_'
+
+
+        strr = re.search('DEFAULT_BASE_DOWNLOAD_URL = .+;', js).group()
+        #print re.findall("'([^']+)'", strr)
+        url = '%s%s%s%s%s'%(re.findall("'([^']+)'", strr)[0],filemark,
+                            re.findall("'([^']+)'", strr)[1],usermark,
+                            re.findall("'([^']+)'", strr)[2])
+        print url
+
+
+        # with PyV8.JSContext() as ctxt:
+        #     ctxt.eval(js)
+        #     vars = ctxt.locals
+        #     var_ex1 = vars.fileMark
+        #     print var_ex1
 
 
 
