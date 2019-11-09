4342bebeeab1af86507a049b51af0818fe90f8de
liugang <335762225@qq.com>
2019-11-09 18:51:49 +0800

    go on with yiyirequests

--- YiyiRequests.py
+++ YiyiRequests.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 import requests
+import urlparse
+import os
+import codecs
 from bs4 import BeautifulSoup
 import scrapy
 from scrapy.selector import Selector
@@ -18,38 +21,61 @@ def yiyi_get(url):
     r = s.get(url=url, headers=headers)
     return r
 
+def parse_url_host(url):
+    host = urlparse.urlsplit(url).netloc
+    ralative_path = os.path.dirname(urlparse.urlsplit(url).path)
+    if ralative_path != '/':
+        host = host + ralative_path
+    return host
+
+
+def fuck():
+    card_url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhoq5pufvqncqv60v6.fgzi.wap.gxlib.org/book/card?cnFenlei=TS273-64&ssid=13677499&d=6f154baf7ec60892601776d79c6cd62a&isFromBW=false&isjgptjs=false'
+
+    r = yiyi_get(card_url)
+    span = Selector(response=r).css('span.zli_i_web')[0]
+    ahref = span.css('a::attr(href)').get()
+    netloc = urlparse.urlparse(card_url).netloc
+    scheme = urlparse.urlparse(card_url).scheme
+
+    jpathreader_url = '%s://%s%s'%(scheme,netloc,ahref)
+    print jpathreader_url
+
+
+    r = yiyi_get(jpathreader_url)
+    print r.url
+    with codecs.open('fuck.html', 'w', encoding='utf-8') as fuck_file:
+        fuck_file.write(r.text)
+    exit()
+
 if __name__ == '__main__':
-    url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhwn0wvqonnnc66690.fgzi.wap.gxlib.org/book/card?cnFenlei=TS273&ssid=13610233&d=3e7583ba5fb01492eaf243482dcf1690&isFromBW=true'
+    fuck()
 
-    book_url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhqwbn0xfqpxuv6wkk.fgzi.wap.gxlib.org/book/card?cnFenlei=TS273&ssid=14424477&d=0e2a9c041ca6a37782e6b74ffb90e993&isFromBW=true&isjgptjs=false'
-    book_url2 = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhqwbn0xfqpxuv6wkk.fgzi.wap.gxlib.org/book/card?cnFenlei=TS273&ssid=13610233&d=3e7583ba5fb01492eaf243482dcf1690&isFromBW=true&isjgptjs=false'
+    exit()
+    html = ''
+    with codecs.open('fuck.html', 'r', encoding='utf-8') as fuck_file:
+        html = fuck_file.read()
 
-    # r = yiyi_get(book_url2)
-    # span = Selector(response=r).css('span.zli_i_web')[0]
-    # print span.css('a::attr(href)').get()
+    #selector = Selector(response=r)
+    selector = Selector(text=html)
 
-    # pdfreader_url = 'http://ffhgfc36ddccdc234f5bb9216d41432f11ddhqwbn0xfqpxuv6wkk.fgzi.wap.gxlib.org/reader/pdf/pdfreader?ssid=96178226&d=791d243c6fd684058ff733a50dd4af24'
-    # r = yiyi_get(pdfreader_url)
-    # span = Selector(response=r).css('span.zli_i_web')[0]
+    scripts = selector.css('script')
 
-    with open('test.html', 'r') as html_file:
-        html_text = html_file.read()
-        selector = Selector(text=html_text)
-        scripts = selector.css('script')
-        js = scripts[3].extract()[8:-9].strip()
+    print scripts[3].extract()
+    js = scripts[3].extract()[8:-9].strip()
 
-        filemark = re.search('var fileMark = "\d*";', js).group()[16:-2]
-        #print filemark
-        usermark = re.search('var userMark = "\d*";', js).group()[16:-2]
-        #print '_'+usermark+'_'
+    filemark = re.search('var fileMark = "\d*";', js).group()[16:-2]
+    #print filemark
+    usermark = re.search('var userMark = "\d*";', js).group()[16:-2]
+    #print '_'+usermark+'_'
 
 
-        strr = re.search('DEFAULT_BASE_DOWNLOAD_URL = .+;', js).group()
-        #print re.findall("'([^']+)'", strr)
-        url = '%s%s%s%s%s'%(re.findall("'([^']+)'", strr)[0],filemark,
-                            re.findall("'([^']+)'", strr)[1],usermark,
-                            re.findall("'([^']+)'", strr)[2])
-        print url
+    strr = re.search('DEFAULT_BASE_DOWNLOAD_URL = .+;', js).group()
+    #print re.findall("'([^']+)'", strr)
+    url = '%s%s%s%s%s'%(re.findall("'([^']+)'", strr)[0],filemark,
+                        re.findall("'([^']+)'", strr)[1],usermark,
+                        re.findall("'([^']+)'", strr)[2])
+    print url
 
 
         # with PyV8.JSContext() as ctxt:
