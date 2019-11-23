ec7a767d0431044c69228700f4f90d24c97f9979
liugang <335762225@qq.com>
2019-11-24 01:55:40 +0800

    123321

--- YiyiSqlite3.py
+++ YiyiSqlite3.py
@@ -4,35 +4,46 @@ import sqlite3
 import os
 
 
+# # This is the qmark style:
+# cur.execute("insert into people values (?, ?)", (who, age))
+#
+# # And this is the named style:
+# cur.execute("select * from people where name_last=:who and age=:age", {"who": who, "age": age})
+
 class Sqlite3db:
     def __init__(self, sqlite_file):
+        self.sqlite_file = sqlite_file
         self.conn = sqlite3.connect(sqlite_file)
         self.conn.text_factory = str
+        # 让查询结果可以用字段名来查询 row['book_id']=row[0]
+        self.conn.row_factory = sqlite3.Row
         self.c = self.conn.cursor()
 
     def __del__(self):
         self.conn.close()
 
-    def executeUpdate(self, sql, ob):
-        """
-        数据库的插入、修改函数
-        :param sql: 传入的SQL语句
-        :param ob: 传入数据
-        :return: 返回操作数据库状态
-        """
-        try:
-            self.c.executemany(sql, ob)
-            i = self.conn.total_changes
-        except Exception as e:
-            print('错误类型： ', e)
-            return False
-        finally:
-            self.conn.commit()
-        if i > 0:
-            return True
+    # select操作，返回全部数据
+    def select(self, sql, parms=None):
+        if parms:
+            self.c.execute(sql, parms)
         else:
-            return False
+            self.c.execute(sql)
+        return self.c.fetchall()
 
-# sqlite3db = Sqlite3db()
-if __name__ == '__main__':
-    sq = Sqlite3db(':memory:')
+    # insert, update, delet操作
+    def execute(self, sql, parms=None):
+        if parms:
+            self.c.execute(sql, parms)
+        else:
+            self.c.execute(sql)
+        self.conn.commit()
+
+    # 建表sql，执行多条sql
+    def create_table(self, sql):
+        self.c.executescript(sql)
+        # print "### SQL:executescript:%s" % sql
+        self.conn.commit()
+
+sqlite3db = Sqlite3db('./data/db/fuck.db')
+
+# 使用 from YiyiSqlite3 import sqlite3db
--- items.py
+++ items.py
@@ -6,23 +6,55 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from YiyiSqlite3 import Sqlite3db
+from YiyiSqlite3 import sqlite3db
+
 
 # 原则：数据全部为字符串，list，dict等转换成json字符串
 
 class YiyiItem(scrapy.Item):
-    def select(self, sql):
+    table_name = None
+
+    def create_table(self):
+        for sb in self.fields:
+            print sb
+
+    @classmethod
+    def select(cls):
+        print dir(cls)
+        yiyiItems = []
+        for row in sqlite3db.select('select * from %s limit 10 ' % cls.table_name):
+            yiyiItem = cls.__init__()
+            print yiyiItem
+
+            exit()
+            for col_names in row.keys():
+                # print col_names
+                yiyiItem[col_names] = row[col_names]
+            yiyiItems.append(yiyiItem)
+        return yiyiItems
 
 
 # select insert update delete
-class XinyuBook(scrapy.Item):
-    bookid = scrapy.Field()
+class XinyuBook(YiyiItem):
+    book_id = scrapy.Field()
     detail_url = scrapy.Field()
     full_detail_url = scrapy.Field()
     title = scrapy.Field()
     img_url = scrapy.Field()
     tags = scrapy.Field()
     cids = scrapy.Field()
+    table_name = 'xinyu_book'
+
+    # @classmethod
+    # def select(cls):
+    #     xinyuBooks = []
+    #     for row in sqlite3db.select('select * from xinyu_book limit 10 '):
+    #         xinyuBook = XinyuBook()
+    #         for col_names in row.keys():
+    #             # print col_names
+    #             xinyuBook[col_names] = row[col_names]
+    #         xinyuBooks.append(xinyuBook)
+    #     return xinyuBooks
 
 
 class ChaoxingBook(scrapy.Item):
@@ -40,5 +72,5 @@ class ChaoxingBook(scrapy.Item):
 
 
 if __name__ == '__main__':
-    item = XinyuBook()
-    item['bookid'] = '123'
+    sb = XinyuBook.select()
+
